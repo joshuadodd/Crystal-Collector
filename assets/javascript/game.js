18aa8f426bc98cc9f871d9fcdd4d80bbc1000c34
Josh Dodd <jdodds@Joshs-MacBook-Pro.local>
2017-06-15 12:29:19 -0400

    working checkpoint

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,45 +1,76 @@
-/*There will be four crystals displayed as buttons on the page.
-The player will be shown a random number at the start of the game.
-When the player clicks on a crystal, it will add a specific amount 
-of points to the player's total score.
-Your game will hide this amount until the player clicks a crystal.
-When they do click one, update the player's score counter.
-The player wins if their total score matches the random number from 
-the beginning of the game.
-The player loses if their score goes above the random number.
-The game restarts whenever the player wins or loses.
-When the game begins again, the player should see a new random 
-number. Also, all the crystals will have four new hidden values. 
-Of course, the user's score (and score counter) will reset to zero.
-The app should show the number of games the player wins and loses. 
-To that end, do not refresh the page as a means to restart the game.
-*/
-<h1>Number to Guess: <span id="number-to-guess"></span></h1>
-  <div id="crystals">
+var wins = 0;
+var losses = 0;
 
-    <img class="crystal-image" src="http://cdn.playbuzz.com/cdn/35910209-2844-45c0-b099-f4d82878d54f/00261fda-4062-4096-81fd-8cf96b9034e8.jpg" alt="crystals">
+var math = 0;
 
-  </div>
+/**Random Number**/
+var number = Math.floor((Math.random() * 100) + 19);
 
-var targetNumber = 50;
+/**Random Crystal Power**/
+var crystal1 = Math.floor((Math.random() * 12) + 1);
+var crystal2 = Math.floor((Math.random() * 12) + 1);
+var crystal3 = Math.floor((Math.random() * 12) + 1);
+var crystal4 = Math.floor((Math.random() * 12) + 1);
 
-  $("#number-to-guess").text(targetNumber);
+var configmath = function() {
+    $('#wins').empty();
+    $('#wins').append(wins);
+    $('#losses').empty();
+    $('#losses').append(losses);
+    $('.math').empty();
+    $('.math').append(math);
 
-  var counter = 0;
-  $(".crystal-image").on("click", function() {
+}
 
-    counter += 10;
+var reset = function() {
+    math = 0;
+    number = Math.floor((Math.random() * 100) + 19);
 
-    alert("New score: " + counter);
+    $('.number').empty();
+    $('.number').append(number);
 
-    if (counter === targetNumber) {
+    crystal1 = Math.floor((Math.random() * 12) + 1);
+    crystal2 = Math.floor((Math.random() * 12) + 1);
+    crystal3 = Math.floor((Math.random() * 12) + 1);
+    crystal4 = Math.floor((Math.random() * 12) + 1);
+    configmath();
+}
 
-      alert("You win!");
+var config = function() {
+    if (math == number) {
+        wins = wins + 1;
+        reset();
+    } else if (math > number) {
+        losses = losses + 1;
+        reset();
+    } else {
+        configmath();
     }
+}
 
-    // Here we added an "else if" condition. If the user's counter ever exceeds the targetNumber...
-    else if (counter >= targetNumber) {
+$('#playerScore').html(math);
+$('#targetNumber').html(number);
 
-      // Then they are alerted with a loss.
-      alert("You lose!!");
-    }
+$(document).ready(function() {
+    $('#crystal1').click(function() {
+        math = math + crystal2;
+        config();
+        
+    })
+    $('#crystal2').click(function() {
+        math = math + crystal1;
+        config();
+        
+
+    })
+    $('#crystal3').click(function() {
+        math = math + crystal3;
+        config();
+        
+    })
+    $('#crystal4').click(function() {
+        math = math + crystal4;
+        config();
+        
+    })
+});
